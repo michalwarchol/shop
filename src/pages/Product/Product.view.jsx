a0761dde86c84c71795edc9a86b04784fc9d1dff
p1rv <karol.p.krol@gmail.com>
2023-06-15 11:08:35 +0200

    fixed horizontal overflow

--- src/pages/Product/Product.view.jsx
+++ src/pages/Product/Product.view.jsx
@@ -5,13 +5,14 @@ import { useQuery } from "src/hooks/useQuery";
 import ProductInfo from "components/ProductInfo/ProductInfo";
 import FilterBar from "components/FilterBar/FilterBar";
 import ProductReviews from "components/ProductReviews/ProductReviews";
+import styles from "./Product.styles.scss";
 
 const ProductView = () => {
   const query = useQuery();
   const id = query.get("id");
 
   return (
-    <div>
+    <div className={styles.product}>
       <Header />
       <FilterBar />
       <ProductInfo id={id} />
